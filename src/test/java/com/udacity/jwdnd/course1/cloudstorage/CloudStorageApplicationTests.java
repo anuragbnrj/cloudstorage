bd1a0605154e8cc1bc94e9f9ff85d2a57f9d2336
Anurag <anuragbnrj@gmail.com>
2021-03-31 23:03:58 +0530

    add integration tests for testing functionality associated with files, credentials and notes

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,112 +1,451 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.pageObject.HomePage;
 import com.udacity.jwdnd.course1.cloudstorage.pageObject.LoginPage;
+import com.udacity.jwdnd.course1.cloudstorage.pageObject.ResultPage;
 import com.udacity.jwdnd.course1.cloudstorage.pageObject.SignupPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
-	private static final String TEST_USER_FIRST_NAME = "John";
-	private static final String TEST_USER_LAST_NAME = "Doe";
-	private static final String TEST_USER_USERNAME = "johndoe";
-	private static final String TEST_USER_PASSWORD = "123456";
+    private static final String TEST_USER_FIRST_NAME = "John";
+    private static final String TEST_USER_LAST_NAME = "Doe";
+    private static final String TEST_USER_USERNAME = "johndoe";
+    private static final String TEST_USER_PASSWORD = "123456";
 
-	@LocalServerPort
-	private int port;
+    private static final String TEST_NOTE_1_TITLE = "Note_1_Title";
+    private static final String TEST_NOTE_1_DESCRIPTION = "Note_1_Description";
+    private static final String TEST_NOTE_1_TITLE_EDITED = "Note_1_Title_Edited";
+    private static final String TEST_NOTE_1_DESCRIPTION_EDITED = "Note_1_Description_Edited";
 
-	public static WebDriver driver;
+    private static final String TEST_CREDENTIAL_1_URL = "Credential_1_Url";
+    private static final String TEST_CREDENTIAL_1_USERNAME = "Credential_1_Username";
+    private static final String TEST_CREDENTIAL_1_PASSWORD = "Credential_1_Password";
+    private static final String TEST_CREDENTIAL_1_URL_EDITED = "Credential_1_Url_Edited";
+    private static final String TEST_CREDENTIAL_1_USERNAME_EDITED = "Credential_1_Username_Edited";
+    private static final String TEST_CREDENTIAL_1_PASSWORD_EDITED = "Credential_1_Password_Edited";
 
-	public String baseURL;
+    @Autowired
+    private UserMapper userMapper;
 
-	@BeforeAll
-	static void beforeAll() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	}
+    @LocalServerPort
+    private int port;
 
-	@BeforeEach
-	public void beforeEach() {
-		baseURL = "http://localhost:" + port;
-	}
+    private static WebDriver driver;
+    private static LoginPage loginPage;
+    private static SignupPage signupPage;
+    private static HomePage homePage;
+    private static ResultPage resultPage;
+    private static String baseURL;
 
-	@AfterAll
-	static void afterAll() {
-		driver.quit();
-		driver = null;
-	}
+    @BeforeAll
+    static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        signupPage = new SignupPage(driver);
+        homePage = new HomePage(driver);
+        resultPage = new ResultPage(driver);
+    }
 
-	@Test
-	public void testHomePageInaccessibleWithoutLogin() {
-		// given
-		String url = baseURL + "/home";
-		// when
-		driver.get(url);
-		// then
-		assertThat(driver.getTitle()).isNotEqualTo("Home");
-		assertThat(driver.getTitle()).isEqualTo("Login");
-		assertThat(driver.getCurrentUrl()).contains("/login");
-	}
+    @BeforeEach
+    void setUp() {
+        baseURL = "http://localhost:" + port;
+    }
 
-	@Test
-	public void testSignupAndLogoutFlow() throws InterruptedException {
-		// given
-		String signupURL = baseURL + "/signup";
-		// when
-		driver.get(signupURL);
-		// then
-		assertThat(driver.getTitle()).isEqualTo("Sign Up");
-		assertThat(driver.getCurrentUrl()).contains("/signup");
+    @AfterEach
+    void tearDown() {
+        userMapper.deleteUserByUsername(TEST_USER_USERNAME);
+    }
 
-		// given
-		SignupPage signupPage = new SignupPage(driver);
-		// when
-		signupPage.signup(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
-		// then
-		WebElement successMessage = signupPage.getSuccessMessage();
-		assertThat(successMessage.getText()).isEqualTo("You successfully signed up! Please continue to the login page.");
+    @AfterAll
+    static void afterAll() {
+        driver.quit();
+        driver = null;
+    }
 
-		// given
-		String loginURL = baseURL + "/login";
-		// when
-		driver.get(loginURL);
-		// then
-		assertThat(driver.getTitle()).isEqualTo("Login");
-		assertThat(driver.getCurrentUrl()).contains("/login");
+    @Test
+    void testHomePageInaccessibleWithoutLogin() {
+        // given
+        // when
+        homePage.tryHomePageUrl(baseURL, driver);
 
-		// given
-		LoginPage loginPage = new LoginPage(driver);
-		// when
-		loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
-		// then
-		assertThat(driver.getTitle()).isEqualTo("Home");
-		assertThat(driver.getCurrentUrl()).contains("/home");
+        // then
+        assertThat(driver.getTitle()).isNotEqualTo("Home");
+        assertThat(driver.getTitle()).isEqualTo("Login");
+        assertThat(driver.getCurrentUrl()).contains("/login");
+    }
 
-		// given
-		HomePage homePage = new HomePage(driver);
-		// when
-		homePage.logout();
-		// then
-		assertThat(driver.getTitle()).isEqualTo("Login");
-		assertThat(driver.getCurrentUrl()).contains("/login");
+    @Test
+    void testSignupAndLogoutFlow() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
 
-		// given
-		String homeURL = baseURL + "/home";
-		// when
-		driver.get(homeURL);
-		// then
-		assertThat(driver.getTitle()).isEqualTo("Login");
-		assertThat(driver.getCurrentUrl()).contains("/login");
-	}
+        // when
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        // then
+        assertThat(driver.getTitle()).isEqualTo("Home");
+        assertThat(driver.getCurrentUrl()).contains("/home");
+
+        // when
+        homePage.logout();
+        // then
+        assertThat(driver.getTitle()).isEqualTo("Login");
+        assertThat(driver.getCurrentUrl()).contains("/login");
+
+        // given
+        // when
+        homePage.tryHomePageUrl(baseURL, driver);
+        // then
+        assertThat(driver.getTitle()).isEqualTo("Login");
+        assertThat(driver.getCurrentUrl()).contains("/login");
+    }
+
+    @Test
+    void testCreateNoteFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // click create note button
+        homePage.clickCreateNoteButton();
+
+        // create new note
+        homePage.createOrEditNote(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertTrue(homePage.isNoteListed(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION));
+
+        // delete note so that user can be cleared from db for other tests
+        homePage.deleteNote(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // logout user
+        homePage.logout();
+    }
+
+    @Test
+    void testEditNoteFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // click create note button
+        homePage.clickCreateNoteButton();
+
+        // create new note
+        homePage.createOrEditNote(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertTrue(homePage.isNoteListed(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION));
+
+        // logout user
+        homePage.logout();
+
+        // visit login page
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // click on edit note button for existing note
+        homePage.clickEditNoteButton(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // fill in new details in existing note
+        homePage.createOrEditNote(TEST_NOTE_1_TITLE_EDITED, TEST_NOTE_1_DESCRIPTION_EDITED);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertTrue(homePage.isNoteListed(TEST_NOTE_1_TITLE_EDITED, TEST_NOTE_1_DESCRIPTION_EDITED));
+
+        // delete note so that user can be cleared from db for other tests
+        homePage.deleteNote(TEST_NOTE_1_TITLE_EDITED, TEST_NOTE_1_DESCRIPTION_EDITED);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // logout user
+        homePage.logout();
+    }
+
+    @Test
+    void testDeleteNoteFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // click create note button
+        homePage.clickCreateNoteButton();
+
+        // create new note
+        homePage.createOrEditNote(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertTrue(homePage.isNoteListed(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION));
+
+        // logout user
+        homePage.logout();
+
+        // visit login page
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertTrue(homePage.isNoteListed(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION));
+
+        // delete note
+        homePage.deleteNote(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open notes tab
+        homePage.openNotesTab();
+
+        // verify new note exists
+        assertFalse(homePage.isNoteListed(TEST_NOTE_1_TITLE, TEST_NOTE_1_DESCRIPTION));
+
+        // logout user
+        homePage.logout();
+    }
+
+    @Test
+    void testCreateCredentialFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // click create credential button
+        homePage.clickCreateCredentialButton();
+
+        // create new credential
+        homePage.createOrEditCredential(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME, TEST_CREDENTIAL_1_PASSWORD);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // verify new credential exists
+        assertTrue(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME));
+
+        // delete credential so that user can be cleared from db for other tests
+        homePage.deleteCredential(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // logout user
+        homePage.logout();
+    }
+
+    @Test
+    void testEditCredentialFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // click create credential button
+        homePage.clickCreateCredentialButton();
+
+        // create new credential
+        homePage.createOrEditCredential(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME, TEST_CREDENTIAL_1_PASSWORD);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // verify new credential exists
+        assertTrue(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME));
+
+        // logout user
+        homePage.logout();
+
+        // visit login page
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // click on edit credential button for existing credential
+        homePage.clickEditCredentialButton(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME);
+
+        // fill in new details in existing credential
+        homePage.createOrEditCredential(TEST_CREDENTIAL_1_URL_EDITED, TEST_CREDENTIAL_1_USERNAME_EDITED, TEST_CREDENTIAL_1_PASSWORD_EDITED);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // verify old credential does not exist
+        assertFalse(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME));
+        // verify edited credential exists
+        assertTrue(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL_EDITED, TEST_CREDENTIAL_1_USERNAME_EDITED));
+
+        // delete credential so that user can be cleared from db for other tests
+        homePage.deleteCredential(TEST_CREDENTIAL_1_URL_EDITED, TEST_CREDENTIAL_1_USERNAME_EDITED);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // logout user
+        homePage.logout();
+    }
+
+    @Test
+    void testDeleteCredentialFunctionality() {
+        signupPage.visitAndVerifySignupPage(baseURL, driver);
+        signupPage.signupTestUserAndVerifySuccessMessage(TEST_USER_FIRST_NAME, TEST_USER_LAST_NAME, TEST_USER_USERNAME, TEST_USER_PASSWORD);
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // click create credential button
+        homePage.clickCreateCredentialButton();
+
+        // create new credential
+        homePage.createOrEditCredential(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME, TEST_CREDENTIAL_1_PASSWORD);
+
+        // verify result page
+        assertThat(driver.getTitle()).isEqualTo("Result");
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // verify new credential exists
+        assertTrue(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME));
+
+        // logout user
+        homePage.logout();
+
+        // visit login page
+        loginPage.visitAndVerifyLoginPage(baseURL, driver);
+
+        // login with test user
+        loginPage.login(TEST_USER_USERNAME, TEST_USER_PASSWORD);
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // delete credential
+        homePage.deleteCredential(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME);
+
+        // click home on result page
+        resultPage.clickAlertSuccessHome();
+
+        // open credentials tab
+        homePage.openCredentialsTab();
+
+        // verify deleted credential does not exist
+        assertFalse(homePage.isCredentialListed(TEST_CREDENTIAL_1_URL, TEST_CREDENTIAL_1_USERNAME));
+
+        // logout user
+        homePage.logout();
+    }
+
 
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/HomePage.java
@@ -1,23 +1,30 @@
 package com.udacity.jwdnd.course1.cloudstorage.pageObject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class HomePage {
+
     @FindBy(css = "#buttonLogout")
     private WebElement logoutButton;
 
+    // Beginning of notes tab elments
     @FindBy(css = "#nav-notes-tab")
     private WebElement notesTab;
 
-    @FindBy(css = "#buttonAddNewNote")
-    private WebElement addNoteButton;
+    @FindBy(css = "#addNewNoteButton")
+    private WebElement addNewNoteButton;
 
-    @FindBy(css = "#saveNoteButton")
-    private WebElement saveNoteButton;
+    @FindBy(className = "noteRow")
+    private List<WebElement> noteRowList;
 
     @FindBy(css = "#note-title")
     private WebElement noteTitleField;
@@ -25,11 +32,47 @@ public class HomePage {
     @FindBy(css = "#note-description")
     private WebElement noteDescriptionField;
 
-    private JavascriptExecutor javascriptExecutor;
+    @FindBy(css = "#saveNoteButton")
+    private WebElement saveNoteButton;
+    // End of notes tab elements
+
+    // Beginning of credential tab elements
+    @FindBy(css = "#nav-credentials-tab")
+    private WebElement credentialsTab;
+
+    @FindBy(css = "#addNewCredentialButton")
+    private WebElement addNewCredentialButton;
+
+    @FindBy(className = "credentialRow")
+    private List<WebElement> credentialRowList;
+
+    @FindBy(css = "#credential-url")
+    private WebElement credentialUrlField;
+
+    @FindBy(css = "#credential-username")
+    private WebElement credentialUsernameField;
+
+    @FindBy(css = "#credential-password")
+    private WebElement credentialPasswordField;
+
+    @FindBy(css = "#saveCredentialButton")
+    private WebElement saveCredentialButton;
+    // End of credential tab elements
+
+    private final JavascriptExecutor javascriptExecutor;
+    private final WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         javascriptExecutor = (JavascriptExecutor) driver;
+        wait = new WebDriverWait(driver, 3);
+    }
+
+    public void tryHomePageUrl(String baseURL, WebDriver driver) {
+        // given
+        String url = baseURL + "/home";
+        // when
+        driver.get(url);
     }
 
     public void logout() {
@@ -37,22 +80,127 @@ public class HomePage {
     }
 
     public void openNotesTab() {
-        this.javascriptExecutor.executeScript("arguments[0].click()", notesTab);
+        wait.until(ExpectedConditions.visibilityOfAllElements(notesTab));
+        javascriptExecutor.executeScript("arguments[0].click()", notesTab);
     }
 
-    public void createNote(String title, String description) {
-        openNotesTab();
+    public void openCredentialsTab() {
+        wait.until(ExpectedConditions.visibilityOfAllElements(credentialsTab));
+        javascriptExecutor.executeScript("arguments[0].click()", credentialsTab);
+    }
 
-        this.addNoteButton.click();
+    public void clickCreateNoteButton() {
+        // open add note modal
+        wait.until(ExpectedConditions.elementToBeClickable(addNewNoteButton)).click();
+    }
 
-        this.noteTitleField.sendKeys(title);
-        this.noteDescriptionField.sendKeys(description);
+    public void clickCreateCredentialButton() {
+        // open add credential modal
+        wait.until(ExpectedConditions.elementToBeClickable(addNewCredentialButton)).click();
+    }
 
-        this.saveNoteButton.click();
+    public void clickEditNoteButton(String title, String description) {
+        // open edit note modal
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewNoteButton));
+        for (WebElement noteRow : noteRowList) {
+            WebElement titleElement = noteRow.findElement(By.className("noteTitle"));
+            WebElement descriptionElement = noteRow.findElement(By.className("noteDescription"));
+            if (titleElement.getText().equals(title) && descriptionElement.getText().equals(description)) {
+                WebElement deleteNoteButton = noteRow.findElement(By.className("editNoteButton"));
+                deleteNoteButton.click();
+            }
+        }
+    }
+
+    public void clickEditCredentialButton(String url, String username) {
+        // open edit credential modal
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewCredentialButton));
+        for (WebElement credentialRow : credentialRowList) {
+            WebElement urlElement = credentialRow.findElement(By.className("credentialUrl"));
+            WebElement usernameElement = credentialRow.findElement(By.className("credentialUsername"));
+            if (urlElement.getText().equals(url) && usernameElement.getText().equals(username)) {
+                WebElement deleteNoteButton = credentialRow.findElement(By.className("editCredentialButton"));
+                deleteNoteButton.click();
+            }
+        }
+    }
+
+    public void createOrEditNote(String title, String description) {
+        // fill note details
+        wait.until(ExpectedConditions.visibilityOf(noteTitleField));
+        noteTitleField.clear();
+        noteTitleField.sendKeys(title);
+        noteDescriptionField.clear();
+        noteDescriptionField.sendKeys(description);
+
+        // save note
+        wait.until(ExpectedConditions.elementToBeClickable(saveNoteButton)).click();
+    }
+
+    public void createOrEditCredential(String url, String username, String password) {
+        // fill credential details
+        wait.until(ExpectedConditions.visibilityOf(credentialUrlField));
+        credentialUrlField.clear();
+        credentialUrlField.sendKeys(url);
+        credentialUsernameField.clear();
+        credentialUsernameField.sendKeys(username);
+        credentialPasswordField.clear();
+        credentialPasswordField.sendKeys(password);
+
+        // save credential
+        wait.until(ExpectedConditions.elementToBeClickable(saveCredentialButton)).click();
     }
 
     public boolean isNoteListed(String title, String description) {
-        return this.notesTab.getText().contains(title) &&
-                this.notesTab.getText().contains(description);
+        // check whether note with given title and description exists or not
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewNoteButton));
+        boolean isNoteListed = false;
+        for (WebElement noteRow : noteRowList) {
+            WebElement titleElement = noteRow.findElement(By.className("noteTitle"));
+            WebElement descriptionElement = noteRow.findElement(By.className("noteDescription"));
+            if (titleElement.getText().equals(title) && descriptionElement.getText().equals(description)) {
+                isNoteListed = true;
+            }
+        }
+        return isNoteListed;
     }
+
+    public boolean isCredentialListed(String url, String username) {
+        // check whether credential with given url and username exists or not
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewCredentialButton));
+        boolean isCredentialListed = false;
+        for (WebElement credentialRow : credentialRowList) {
+            WebElement urlElement = credentialRow.findElement(By.className("credentialUrl"));
+            WebElement usernameElement = credentialRow.findElement(By.className("credentialUsername"));
+            if (urlElement.getText().equals(url) && usernameElement.getText().equals(username)) {
+                isCredentialListed = true;
+            }
+        }
+        return isCredentialListed;
+    }
+
+    public void deleteNote(String title, String description) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewNoteButton));
+        for (WebElement noteRow : noteRowList) {
+            WebElement titleElement = noteRow.findElement(By.className("noteTitle"));
+            WebElement descriptionElement = noteRow.findElement(By.className("noteDescription"));
+            if (titleElement.getText().equals(title) && descriptionElement.getText().equals(description)) {
+                WebElement deleteNoteButton = noteRow.findElement(By.className("deleteNoteButton"));
+                deleteNoteButton.click();
+            }
+        }
+    }
+
+    public void deleteCredential(String url, String username) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(addNewCredentialButton));
+        for (WebElement credentialRow : credentialRowList) {
+            WebElement urlElement = credentialRow.findElement(By.className("credentialUrl"));
+            WebElement usernameElement = credentialRow.findElement(By.className("credentialUsername"));
+            if (urlElement.getText().equals(url) && usernameElement.getText().equals(username)) {
+                WebElement deleteCredentialButton = credentialRow.findElement(By.className("deleteCredentialButton"));
+                deleteCredentialButton.click();
+            }
+        }
+    }
+
 }
