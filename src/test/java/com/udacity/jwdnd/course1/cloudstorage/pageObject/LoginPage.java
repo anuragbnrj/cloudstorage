b271dd933e4cea42eb986868ecc5f615dee5ae72
Anurag <anuragbnrj@gmail.com>
2021-03-31 23:02:09 +0530

    add more functionality to login page and signup page objects

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/LoginPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/LoginPage.java
@@ -1,9 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.pageObject;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LoginPage {
 
@@ -16,13 +20,28 @@ public class LoginPage {
     @FindBy(css = "#submitButton")
     private WebElement submitButton;
 
-    public LoginPage(WebDriver webDriver) {
-        PageFactory.initElements(webDriver, this);
+    private JavascriptExecutor javascriptExecutor;
+    private final WebDriverWait wait;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        javascriptExecutor = (JavascriptExecutor) driver;
+        wait = new WebDriverWait(driver, 3);
+    }
+
+    public void visitAndVerifyLoginPage(String baseURL, WebDriver driver) {
+        // given
+        String loginURL = baseURL + "/login";
+        // when
+        driver.get(loginURL);
+        // then
+        assertThat(driver.getTitle()).isEqualTo("Login");
+        assertThat(driver.getCurrentUrl()).contains("/login");
     }
 
     public void login(String username, String password) {
-        this.usernameField.sendKeys(username);
-        this.passwordField.sendKeys(password);
-        this.submitButton.click();
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        submitButton.click();
     }
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/SignupPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/pageObject/SignupPage.java
@@ -1,12 +1,14 @@
 package com.udacity.jwdnd.course1.cloudstorage.pageObject;
 
-import lombok.Getter;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-@Getter
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SignupPage {
 
     @FindBy(css = "#inputFirstName")
@@ -27,11 +29,34 @@ public class SignupPage {
     @FindBy(css = "#successMessage")
     private WebElement successMessage;
 
-    public SignupPage(WebDriver webDriver) {
-        PageFactory.initElements(webDriver, this);
+    private JavascriptExecutor javascriptExecutor;
+    private final WebDriverWait wait;
+
+    public SignupPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        javascriptExecutor = (JavascriptExecutor) driver;
+        wait = new WebDriverWait(driver, 3);
     }
 
-    public void signup(String firstName, String lastName, String username, String password) {
+    public void visitAndVerifySignupPage(String baseURL, WebDriver driver) {
+        // given
+        String signupURL = baseURL + "/signup";
+        // when
+        driver.get(signupURL);
+        // then
+        assertThat(driver.getTitle()).isEqualTo("Sign Up");
+        assertThat(driver.getCurrentUrl()).contains("/signup");
+    }
+
+    public void signupTestUserAndVerifySuccessMessage(String fname, String lname, String username, String password) {
+        // given
+        // when
+        signup(fname, lname, username, password);
+        // then
+        assertThat(successMessage.getText()).isEqualTo("You successfully signed up! Please continue to the login page.");
+    }
+
+    private void signup(String firstName, String lastName, String username, String password) {
         this.firstNameField.sendKeys(firstName);
         this.lastNameField.sendKeys(lastName);
         this.usernameField.sendKeys(username);
