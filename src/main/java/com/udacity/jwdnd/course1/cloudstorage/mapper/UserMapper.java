2d9517ecaa5ee8d3f198fd7ae6dabe1dd6a290cf
Anurag <anuragbnrj@gmail.com>
2021-03-31 23:00:42 +0530

    modify UserMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
@@ -14,6 +11,9 @@ public interface UserMapper {
 
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
-    int insert(User user);
+    int insertUser(User user);
+
+    @Delete("DELETE FROM USERS WHERE username = #{username}")
+    int deleteUserByUsername(String username);
 
 }
