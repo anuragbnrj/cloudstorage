1efb22f26f3494393c550a27404b28ff326d622c
Anurag <anuragbnrj@gmail.com>
2021-04-02 02:32:57 +0530

    display message when user logs out

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
+                .logoutSuccessUrl("/login?logout=true")
                 .permitAll();
     }
 
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,15 +1,21 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
     @GetMapping()
-    public String loginView() {
+    public String loginView(@RequestParam(value = "logout", defaultValue = "false") Boolean logout,
+                            Model model) {
+        if (logout) {
+            model.addAttribute("logout", true);
+        }
         return "login";
     }
 
